2ec2fb38d1361d1218bd083b7b6906eccb786a0c
vitalcha <vkrokhov@gmail.com>
2020-01-26 22:03:33 +0200

    donehw11

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -36,5 +36,53 @@ const structure = [
 ];
 
 const rootNode = document.getElementById('root');
+function activeFolder(el, structure) {
+  if (structure) {
+    const ul = document.createElement('ul');
+    for (let item of structure) {
+      const li = document.createElement('li');
+      const el = document.createElement('div');
+      el.classList.add('title');
+      li.appendChild(el);
+      if (item.folder) {
+        li.classList.add('folder');
+        el.addEventListener(
+          'click',
+          event => {
+            event.target.parentNode.classList.toggle('open');
+            if (event.target.parentNode.classList.contains('open')) {
+              event.target.children[0].textContent = 'folder_open';
+            } else {
+              event.target.children[0].textContent = 'folder';
+            }
+            event.preventDefault();
+          },
+          true
+        );
+        let image = document.createElement('i');
+        image.classList.add('material-icons', 'folder');
+        image.textContent = 'folder';
+        el.appendChild(image);
+        el.appendChild(document.createTextNode(item.title));
+        activeFolder(li, item.children);
+      } else {
+        li.classList.add('file');
+        let image = document.createElement('i');
+        image.classList.add('material-icons', 'file');
+        image.appendChild(document.createTextNode('insert_drive_file'));
+        el.appendChild(image);
+        el.appendChild(document.createTextNode(item.title));
+      }
+      ul.appendChild(li);
+    }
+    el.appendChild(ul);
+  } else {
+    let emptyFolder = document.createElement('p');
+    emptyFolder.classList.add('empty-message');
+    const textNode = document.createTextNode('Folder is empty');
+    emptyFolder.appendChild(textNode);
+    el.appendChild(emptyFolder);
+  }
+}
 
-// Todo: your code goes here
+activeFolder(rootNode, structure);
